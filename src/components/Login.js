570a2f9b582ee3c375801a820f0e0318d51257a3
Tanishq Sharma <90607852+tanishqsharma7@users.noreply.github.com>
2023-09-28 21:03:57 +0530

    Signin/SignUp

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,28 +18,28 @@ const Login = () => {
         />
       </div>
       <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">
+        <h1 className="font-bold text-3xl py-4 my-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
             type="text"
             placeholder="Full Name"
-            className="p-4 my-4 w-full bg-gray-800 rounded-sm"
+            className="p-4 my-4 w-full bg-[#333333] rounded-sm"
           />
         )}
         <input
           type="text"
           placeholder="Email Address"
-          className="p-4 my-4 w-full bg-gray-800 rounded-sm"
+          className="p-4 my-4 w-full bg-[#333333] rounded-sm"
         />
 
         <input
           type="password"
           placeholder="Password"
-          className="p-4 my-4 w-full bg-gray-800 rounded-sm"
+          className="p-4 w-full bg-[#333333] rounded-sm"
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+        <button className="p-4 my-6 bg-[#e50914] w-full rounded-lg">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-6 cursor-pointer" onClick={toggleSignInForm}>
